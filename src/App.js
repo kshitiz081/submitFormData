522a24ec8ee70b8c61f14a4cbf994734ec7b20e9
Kumar <kshitiz.kumar@refinitiv.com>
2024-01-23 19:45:06 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
-
+function handleSubmit(e){
+  e.preventDefault();
+  console.log(e.target[0].value);
+  console.log(e.target[1].value);
+  console.log(e.target[2].value);
+  console.log(e.target[3].value);
+  console.log(e.target[4].value);
+  console.log(e.target[5].value);
+  console.log(e.target[6].value);
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit} >
+        <label >Name :
+          <input  name="" id="name" />
+        </label>
+        <br />
+        <label >E-mail : 
+          <input  name="" id="email" />
+        </label>
+        <br />
+        <label >Password :
+          <input  name="" id="password" type='password'/>
+        </label>
+        <br />
+        <label >Gender :
+          <select name="gender" id="">
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+        </label>
+        <br />
+        <label>City :
+          <input  name="" id="city" type='text'/>
+        </label>
+        <br />
+        <label >State :
+          <input  name="" id="state" type='text'/>
+        </label>
+        <br />
+        <label >Country :
+          <input  name="" id="country" type='ext'/>
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
